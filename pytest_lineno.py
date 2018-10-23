e9ced7e7eb7f41d117df2168c62f3fd35f77ff5a
Aleks Kamko <aykamko@gmail.com>
2018-10-22 17:20:04 -0700

    correctly handle last function in class def

--- pytest_lineno.py
+++ pytest_lineno.py
@@ -4,10 +4,10 @@ Invoke tests by linenumber, e.g. foo/bar/baz.py::100
 import re
 import ast
 
-import pytest
+import pytest  # pylint: disable=unused-import
 
 
-LINENO_RE = re.compile(r'^(.+\.py)::(\d+$)')
+LINENO_RE = re.compile(r'^(.+\.py)::?(\d+$)')
 
 
 class GetTargetClassOrFunction(ast.NodeVisitor):
@@ -16,7 +16,7 @@ class GetTargetClassOrFunction(ast.NodeVisitor):
   """
   def __init__(self, target_lineno):
     self.target_lineno = target_lineno
-    self.last_node = ast.ClassDef(lineno=0)  # sentinel value
+    self.last_node = None
     self.target_node_path = None
 
   def _visit_node(self, node, parents=None):
@@ -28,10 +28,13 @@ class GetTargetClassOrFunction(ast.NodeVisitor):
     self.last_node = node
 
   def visit_ClassDef(self, node):
+    self.last_node = node
     self._visit_node(node)
     for cnode in ast.iter_child_nodes(node):
       if isinstance(cnode, ast.FunctionDef):
         self._visit_node(cnode, parents=[node])
+    # check if target is in the last function def
+    self._visit_node(ast.Pass(lineno=float('inf')), parents=[node])
 
   def visit_FunctionDef(self, node):
     self._visit_node(node)
